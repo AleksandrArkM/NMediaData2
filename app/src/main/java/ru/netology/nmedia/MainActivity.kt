56ff3955b2625315c9f0321b5e1f5f9354d930c1
Alexander M <melua.aleksandr@gmail.com>
2022-08-29 14:54:53 +0300

    commit end

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,8 @@ import android.util.Log
 import android.view.View
 import androidx.activity.result.launch
 import androidx.activity.viewModels
+import androidx.core.content.edit
+import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.util.hideKeyboard
@@ -21,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        run {
+            val preferences = getPreferences(Context.MODE_PRIVATE)
+            preferences.edit{
+                putString("key", "value")
+            }
+        }
+
+        run{
+            val preferences = getPreferences(Context.MODE_PRIVATE)
+            val value = preferences.getString("key", "no value") ?: return@run
+            Snackbar.make(binding.root, value, Snackbar.LENGTH_INDEFINITE).show()
+        }
 
         val adapter = PostsAdapter(viewModel)
 
